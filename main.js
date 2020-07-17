97ae5906ca3f35a89e3341e256905b97959d4481
dimazhev <dzhevelev@aol.com>
2020-07-17 12:51:20 -0700

    Initial

---
+++ main.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function () {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+    $('.top-nav .nav-link').on('click', function () {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000);
+    });
+
+    $('#up').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 2000);
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    });
+
+    $(document).ready(function(){
+        $('.submit').click(function(event){
+            event.preventDefault()
+            console.log('clicked button')
+
+            var email= $('.name').val()
+            var email= $('.email').val()
+            var email= $('.services').val()
+            var email= $('.subject').val()
+
+            
+        })
+    })
+
+});
